4e541031a17e7d444868cce5dc3c1b0819d7e265
Revati <revati.kamannavar@microfocus.com>
2022-01-08 14:28:28 +0530

    Iterator Design Pattern

---
+++ Java/Java_Design_Patterns/Behavioral/src/main/java/Iterator/IteratorDemo.java
@@ -0,0 +1,12 @@
+package Iterator;
+
+public class IteratorDemo {
+    public static void main(String[] args) {
+        NameList names= new NameList();
+        Iterator iterator = names.getIterator();
+        while(iterator.hasNext())
+        {
+            System.out.println(iterator.next());
+        }
+    }
+}
