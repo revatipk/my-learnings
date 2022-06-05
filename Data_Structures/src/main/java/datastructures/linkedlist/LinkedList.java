798adc6a54b12689dcc64f4ee408c1277249a497
Revati <revati.kamannavar@microfocus.com>
2022-06-05 17:17:02 +0530

    LinkedList implementation

---
+++ Data_Structures/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -0,0 +1,102 @@
+package datastructures.linkedlist;
+
+public class LinkedList {
+    Node head;
+    Node tail;
+    int lenght;
+
+    public LinkedList(int value){
+        head = new Node(value);
+        tail = head;
+        lenght =1;
+    }
+    public void append(int value){
+        Node node = new Node(value);
+        tail.next = node;
+        tail = node;
+        lenght ++;
+    }
+    public int getLenght(){
+        return lenght;
+    }
+    public void print(){
+        Node node = head;
+        int i=0;
+        while (i<lenght)
+        {
+            System.out.println(node.value);
+            node = node.next;
+            i++;
+        }
+    }
+    public void insert(int value, int index)
+    {
+        if(index<0 || index>lenght){
+            System.out.println("ARRAY OUT OF BOX");
+        }
+        if(index==0){
+            prepend(value);
+        }
+        else if(index==lenght)
+        {
+            append(value);
+        }
+        else
+        {
+            Node node = head;
+            for(int i=0;i<index-1;i++)
+                node = node.next;
+            Node insert = new Node(value);
+            insert.next = node.next;
+            node.next = insert;
+            lenght++;
+        }
+    }
+    public void prepend(int value){
+        Node node = new Node(value);
+        node.next = head;
+        head = node;
+        lenght++;
+    }
+
+    public void remove(int index)
+    {
+        if(index<0 || index>lenght){
+            System.out.println("ARRAY OUT OF BOX");
+        }
+        else if(index ==0)
+        {
+            Node node = head;
+            head = head.next;
+            node = null;
+            lenght--;
+        }
+        else
+        {
+            Node node = head;
+            int i =0;
+            while (i<index -1)
+            {
+                node = node.next;
+                i++;
+            }
+            node.next = node.next.next;
+            lenght--;
+            if(i==lenght-1)
+                tail = node;
+        }
+    }
+    public static void main(String []args)
+    {
+        LinkedList list = new LinkedList(10);
+        list.append(12);
+        list.append(14);
+        list.append(16);
+        list.print();
+        list.insert(15,3);
+        list.print();
+        list.remove(2);
+        list.print();
+    }
+
+}
---
+++ Data_Structures/src/main/java/datastructures/linkedlist/Node.java
@@ -0,0 +1,10 @@
+package datastructures.linkedlist;
+
+public class Node {
+    int value;
+    Node next;
+    public Node(int input){
+        value = input;
+        next = null;
+    }
+}
